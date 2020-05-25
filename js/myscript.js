aec1ac08b1fa83e493abf695743e92c2ded7dc78
YOGESH BALKRISHNA SALVE <53407767+yogeshsalve@users.noreply.github.com>
2020-05-26 01:31:45 +0530

    setting up environment for Angular JS Project

---
+++ js/myscript.js
@@ -0,0 +1,44 @@
+const URL = "https://covid19.mathdro.id/api";
+
+
+let app = angular.module("MyApp", []);
+
+app.controller('MyCtrl', ($scope, $http)=>{
+
+    $scope.title = "Stay Home Stay Safe"
+
+    console.log("APP Loaded");
+ 
+    // Calling API
+
+    $http.get(URL).then(
+        (response)=>{
+            
+             console.log(response.data);
+             $scope.all_data = response.data
+        },
+        (error)=> {
+            console.log(error);
+        }
+    );
+
+    //get country data
+    $scope.get_c_data = () => {
+        let country=$scope.c;
+        if(country == "") {
+            $scope.c_data=undefined;
+            return;
+        }
+
+        $http.get(`${URL}/countries/${country}`)
+        .then((response)=>{
+            console.log(response.data);
+            $scope.c_data = response.data;
+        },
+        (error)=>{
+            console.log(error);
+        }
+        );
+    };
+
+});
